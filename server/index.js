d6c1d4d270ba243a111b6b21016ac35e80a855d3
Bangaram <kuramdasubangaram@gmail.com>
2026-09-13 19:49:50 +0530

    Improve GPS location name

--- server/index.js
+++ server/index.js
@@ -111,7 +111,7 @@ const lookupGpsLocation = async (latitude, longitude) => {
     const data = await response.json();
 
     return {
-      city: data.city || data.locality || data.principalSubdivision || null,
+      city: data.locality || data.city || data.principalSubdivision || null,
       region: data.principalSubdivision || null,
       country: data.countryName || null,
       district: data.localityInfo?.administrative?.[2]?.name || null,
